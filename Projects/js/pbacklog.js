9a6cb220501786374d6e5687870284116e7b24ff
lphi0011 <lphi0011@student.monash.edu>
2022-08-29 21:40:57 +1000

    Luke: basically completed product backlog mockup

--- Projects/js/pbacklog.js
+++ Projects/js/pbacklog.js
@@ -4,6 +4,8 @@ let view_dialog = document.getElementById('view-dialog')
 let view_button = document.getElementById('open-button')
 let add_dialog = document.getElementById('add-dialog')
 let add_button = document.getElementById('add-button')
+let close_dialog = document.getElementById('close-dialog')
+let close_button = document.getElementById('close-button')
 
 view_button.addEventListener('click', function() {
     view_dialog.showModal();
@@ -19,4 +21,12 @@ add_button.addEventListener('click', function() {
 
 add_dialog.querySelector('.close').addEventListener('click', function() {
     add_dialog.close();
+});
+
+close_button.addEventListener('click', function() {
+    close_dialog.showModal();
+});
+
+close_dialog.querySelector('.close').addEventListener('click', function() {
+    close_dialog.close();
 });
