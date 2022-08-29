8412fb7d81639eab9cec050f0c784d94ce453f02
parulgarg <pgar0011@student.monash.edu>
2022-08-29 22:50:59 +1000

    changed colours of priority in pbacklog.css

--- Sandbox/hello-world_luke.js
+++ Sandbox/hello-world_luke.js
@@ -1,3 +1,5 @@
 "use strict";
 
-console.log("goodbye, world!");
+console.log("hello, world!\n- from luke");
+console.log("plz accept merge request")
+console.log('added new message to give commit example')
