50258ccf917d67b49325fba0faecf82a78581a3f
DASUN_NADEERA_MAHAMADACHCHI <dmah0009@student.monash.edu>
2022-09-02 13:06:23 +1000

    colour functionality for tag and prio

--- Projects/js/pbacklog.js
+++ Projects/js/pbacklog.js
@@ -38,6 +38,36 @@ function showCards(){
     for(let i=0; i<productBacklog.showTasks().length;i++){
         let task = productBacklog.showTasks()[i]
         console.log(task)
+        let prio=task.priority;
+        let tag=task.tags;
+        let prio_css;
+        let tag_css;
+        if(prio=="low"){
+            prio_css="low-p"
+        }
+        else if(prio=="med"){
+            prio_css="med-p"
+        }
+        else if(prio=="high"){
+            prio_css="high-p"
+        }
+        else if(prio=="crit"){
+            prio_css="crit-p"
+        }
+
+        if(tag=="UI"){
+            tag_css= "ui-tag"
+        }
+        else if(tag=="CORE"){
+            tag_css="core-tag"
+        }
+        else if(tag=="Testing"){
+            tag_css="testing-tag"
+        }
+        console.log(prio)
+        console.log(prio_css)
+        console.log(tag)
+        console.log(tag_css)
         words+=
         `<div class="mdl-cell mdl-cell--4-col">
             <div class="demo-card-wide mdl-card mdl-shadow--2dp" id="card1">
@@ -45,10 +75,10 @@ function showCards(){
                     <h2 class="mdl-card__title-text">${task.name}</h2>
                 </div>
                 <div class="mdl-card__supporting-text" style="font-family:Roboto, sans-serif">
-                    <span class="mdl-chip ui-tag">
+                    <span class="mdl-chip ${tag_css}">
                         <span class="mdl-chip__text">${task.tag}</span>
                     </span>
-                    <span class="mdl-chip low-p">
+                    <span class="mdl-chip ${prio_css}">
                         <span class="mdl-chip__text">${task.priority}</span>
                     </span>
                     <span class="mdl-chip">
