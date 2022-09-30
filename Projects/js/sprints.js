81b0ed1e02352d7e3a7e7093f638000f8c3ba419
parulgarg <pgar0011@student.monash.edu>
2022-10-01 07:36:40 +1000

    changed not started and complete sprint colours

--- Projects/js/sprints.js
+++ Projects/js/sprints.js
@@ -540,7 +540,7 @@ function showNotStartedSprint()
             `
             <div class="mdl-cell mdl-cell--4-col">
             <div class="demo-card-wide mdl-card mdl-shadow--2dp" id="notstarted${i}">
-                    <div class="mdl-card__title" style="background: darkgreen">
+                    <div class="mdl-card__title" style="background: orange">
                         <h2 class="mdl-card__title-text">Iteration ${notStartedSprints[i].sprint_id}</h2>
                     </div>
                     <div class="mdl-card__supporting-text" style="font-family:Roboto, sans-serif">
@@ -624,7 +624,7 @@ function showCompletedSprint(){
             `
             <div class="mdl-cell mdl-cell--4-col">
             <div class="demo-card-wide mdl-card mdl-shadow--2dp" id="notstarted${i}">
-                    <div class="mdl-card__title" style="background: darkgreen">
+                    <div class="mdl-card__title" style="background: orange">
                         <h2 class="mdl-card__title-text">Iteration ${completedSprints[i].sprint_id}</h2>
                     </div>
                     <div class="mdl-card__supporting-text" style="font-family:Roboto, sans-serif">
